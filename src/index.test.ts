c522b22c8602144c9b6f5f0802d0cc8f5c7aa25d
Bassel Kanso <basselkanso82@gmail.com>
2022-05-28 16:50:28 +0300

    setting up prettier eslint and husky

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
-import request from 'supertest';
-import {app} from './index';
+import request from "supertest";
+import { app } from "./index";
 
-test('Hello world works', async () => {
-    const response = await request(app.callback()).get('/');
-    expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World');
+test("Hello world works", async () => {
+  const response = await request(app.callback()).get("/");
+  expect(response.status).toBe(200);
+  expect(response.text).toBe("Hello World");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
-import Koa, {Context} from 'koa';
-import Router from 'koa-router';
+import Koa, { Context } from "koa";
+import Router from "koa-router";
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx: Context) => {
-  ctx.body = 'Hello World';
+router.get("/", (ctx: Context) => {
+  ctx.body = "Hello World";
 });
-
 
 app.use(router.routes());
 
 app.listen(process.env.PORT);
 
+// eslint-disable-next-line
 console.info(`Listening to http://localhost:${process.env.PORT} 🚀`);
 
-export {
-  app
-};
+export { app };
